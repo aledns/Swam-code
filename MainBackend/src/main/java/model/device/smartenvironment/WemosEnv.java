df5b4b05fd2a177e2046d3a0fac6fce6d5a3bfdd
aledns <37532285+aledns@users.noreply.github.com>
2020-04-02 19:49:04 +0200

    Add files via upload

--- MainBackend/src/main/java/model/device/smartenvironment/WemosEnv.java
+++ MainBackend/src/main/java/model/device/smartenvironment/WemosEnv.java
@@ -1,6 +1,7 @@
 package model.device.smartenvironment;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
@@ -10,14 +11,14 @@ import model.device.SmartController;
 import model.device.SmartEnvironment;
 
 @Entity
-@DiscriminatorValue("WEMOSENV")
+@DiscriminatorValue("WemosEnv")
 public class WemosEnv extends SmartEnvironment {
 	
 	public WemosEnv() {
 		super();
 	}
 
-	public WemosEnv(String name, double frequency, List<SmartController> smartcontrollers, List<Sensor> sensors) {
+	public WemosEnv(String name, double frequency, Set<SmartController> smartcontrollers, Set<Sensor> sensors) {
 		super(name, frequency, smartcontrollers, sensors);
 	}
 	
